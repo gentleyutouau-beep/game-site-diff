969fc06e0edabae5e9465d9e5f6c2428ea43584b
RickNi <gentleyutou.au@gmail.com>
2026-01-25 09:34:50 +1000

    Type cron API handler params

--- web/src/pages/api/cron.ts
+++ web/src/pages/api/cron.ts
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     res.status(405).send('Method Not Allowed');
     return;
